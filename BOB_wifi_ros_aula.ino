3e87c6afd3144513eea82647bb42fd25909e8af1
Leticia <leticiarp2000@hotmail.com>
2022-12-12 11:11:52 -0300

    troca biblioteca mpu para melhorar a leitura

--- BOB_wifi_ros_aula.ino
+++ BOB_wifi_ros_aula.ino
@@ -157,12 +157,90 @@ void IRAM_ATTR ISR_EC2()
   EC2_count++;
 }
 
+void setup_imu()
+{
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+  
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+        
+    // turn on the DMP
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } 
+    
+    else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(devStatus);
+      Serial.println(F(")"));
+    }
+}
+
+float get_yaw()
+{
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+    
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+
+    // display Euler angles in radians
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t"); // print yaw
+    Serial.print(ypr[0]);
+    Serial.print("\t");
+    Serial.print(ypr[1]);  // print pitch 
+    Serial.print("\t");
+    Serial.println(ypr[2]); // print roll 
+  }
+  // return yaw
+  return ypr[0];
+
+}
+
 void setup() {
 
   configuration.serverPort=configuration.serverPort+configuration.robot;  
   IPAddress server(configuration.serverIP[0], configuration.serverIP[1], configuration.serverIP[2], configuration.serverIP[3]);   // Set the rosserial socket server IP address
   setupWiFi();
-  delay(2000);
+
+  // configure ros communication
   nh.getHardware()->setConnection(server, configuration.serverPort); 
   nh.initNode();
   nh.subscribe(sub_cmd_vel);
@@ -178,77 +256,23 @@ void setup() {
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
   pinMode(LED, OUTPUT);
+
+  // configure servo motor and put it at "position 0"
   servo.attach(SRV);
-  servo.write(90);
+  servo.write(55);
+
+  // configure interruption pins for encoder
   pinMode(EC1, INPUT);
   attachInterrupt(EC1, ISR_EC1, RISING);
   pinMode(EC2, INPUT);
   attachInterrupt(EC2, ISR_EC2, RISING);
 
-  // --- mpu config
-      // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
+  // initialize serial communication  
+  Serial.begin(115200);
+  while (!Serial);
 
-    Serial.begin(115200);
-    while (!Serial);
-
-    // initialize device
-    Serial.println(F("Initializing I2C devices..."));
-    mpu.initialize();
-
-    // verify connection
-    Serial.println(F("Testing device connections..."));
-    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // wait for ready
-    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    while (Serial.available() && Serial.read()); // empty buffer
-    while (!Serial.available());                 // wait for data
-    while (Serial.available() && Serial.read()); // empty buffer again
-
-    // load and configure the DMP
-    Serial.println(F("Initializing DMP..."));
-    devStatus = mpu.dmpInitialize();
-
-    // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(220);
-    mpu.setYGyroOffset(76);
-    mpu.setZGyroOffset(-85);
-    mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
-
-    // make sure it worked (returns 0 if so)
-    if (devStatus == 0) {
-        // Calibration Time: generate offsets and calibrate our MPU6050
-        mpu.CalibrateAccel(6);
-        mpu.CalibrateGyro(6);
-        mpu.PrintActiveOffsets();
-        // turn on the DMP, now that it's ready
-        Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
-
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-        Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        Serial.print(F("DMP Initialization failed (code "));
-        Serial.print(devStatus);
-        Serial.println(F(")"));
-    }
-
-  
+  // setup and calibration of the imu sensor 
+  setup_imu();
 }
 
 void loop() {
@@ -262,23 +286,8 @@ void loop() {
   sharp_msg.data = SharpSensor.distance()*1.8;
   pub_sharp.publish(&sharp_msg);
 
-      // if programming failed, don't try to do anything
-    if (!dmpReady) return;
-    // read a packet from FIFO
-    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
-
-        // display Euler angles in degrees
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        Serial.print("ypr\t");
-        Serial.print(ypr[0]I);
-        Serial.print("\t");
-        Serial.print(ypr[1]);
-        Serial.print("\t");
-        Serial.println(ypr[2]);
-
-    }
+  imu_msg.data = get_yaw(); 
+  pub_imu.publish(&imu_msg); 
 
   nh.spinOnce();
 }
