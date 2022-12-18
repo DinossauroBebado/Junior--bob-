a32eecaf6c1f8a933b29f4671cc3a6d68f5e00e1
leticia <leticiarp2000@hotmail.com>
2022-12-18 09:19:28 -0300

    codigo pid no matlab

--- BOB_wifi_ros_aula/Main_junior.m
+++ BOB_wifi_ros_aula/Main_junior.m
@@ -6,25 +6,30 @@
 
 % ------- config publisher
 msg_twist = rosmessage('geometry_msgs/Twist') ;
-pub_twist = rospublisher("/cmd_vel",'geometry_msgs/Twist');
+pub_twist = rospublisher("/bob/cmd_vel",'geometry_msgs/Twist');
 
 % ------- config subscriber
-sub_odom = rossubscriber("odom");
+sub_odom = rossubscriber("/odom");
 odom_data = receive(sub_odom,10); 
 
+% ------- inicializar com velocidade 0
+msg_twist.Linear.X = 0; 
+msg_twist.Angular.Z = 0; 
+send(pub_twist,msg_twist);
+
 % setpoint xy
-target_x = 3.5;
-target_y = 3.5;
+target_x = 0.4;
+target_y = 0;
 
 % constantes PID linaer 
-kp_linear = 0.5;
-ki_linear = 0.1; 
-kd_linear = 0.1; 
+kp_linear = 0;
+ki_linear = 0; 
+kd_linear = 0; 
 
 % constantes PID angular
 kp_angular = 0.1;
-ki_angular = 0.1; 
-kd_angulat = 0.1; 
+ki_angular = 0;
+kd_angular = 0; 
 
 % variavais para calculo deltaT
 elapsedTime = 0; 
@@ -36,18 +41,25 @@ previous_error_angular = 0;
 vel_linear_integral = 0;
 vel_angular_integral = 0; 
 
-error_linear =  hypot((target_x - odom_data.Pose.Pose.Position.X),(target_y - odom_data.Pose.Pose.Position.Y)); 
+error_linear =  99; 
 error_angular =  99;   % fazer com que o scrip entre na condição while abaixo
 
-while(abs(error_angular) > pi/10 && abs(error_linear) > 1)
+while(abs(error_angular) > pi/10 && abs(error_linear) >= 1/20)
     
+    % recebe dados de odometria
     odom_data = receive(sub_odom,10); 
-        
-    setpoint_angle = atan2((target_x-odom_data.Pose.Pose.Position.X),(target_y-odom_data.Pose.Pose.Position.Y));
-
+    
+    Quaternion_ros = odom_data.Pose.Pose.Orientation ;
+    quat = [odom_data.Pose.Pose.Orientation.X odom_data.Pose.Pose.Orientation.Y odom_data.Pose.Pose.Orientation.Z odom_data.Pose.Pose.Orientation.W];
+       
+    %z in radians de - pi a pi 
+    yaw = quat2angle(quat,'XYZ');
+    
     error_linear =  hypot((target_y - odom_data.Pose.Pose.Position.Y),(target_x - odom_data.Pose.Pose.Position.X));
         
-    error_angular = setpoint_angle - odom_data.Pose.Pose.Orientation.Z;
+    setpoint_angle = atan2((target_x-odom_data.Pose.Pose.Position.X),(target_y-odom_data.Pose.Pose.Position.Y));
+        
+    error_angular = setpoint_angle - yaw;
 
     % condição para complementar o angulo caso necessário
     if(error_angular<0 && odom_data.Pose.Pose.Orientation.Z > 0)
@@ -58,12 +70,12 @@ while(abs(error_angular) > pi/10 && abs(error_linear) > 1)
     elapsedTime = etime(clock, previous_time); 
     
     % ------- integral 
-    vel_linear_integral = (error_linear*ki_linear*elapsedTime) + vel_linear_integral;
-    vel_angular_integral = (error_angular*ki_angular*elapsedTime) + vel_angular_integral; 
+    vel_linear_integral = (error_linear*ki_linear*elapsedTime);
+    vel_angular_integral = (error_angular*ki_angular*elapsedTime); 
 
     % ------- devidada 
-    vel_linear_derivative = (error_linear- previous_error_linear)/elapsedTime; 
-    vel_angular_derivative = (error_angular - previous_error_angular)/elapsedTime; 
+    vel_linear_derivative = kd_linear*(error_linear- previous_error_linear)/elapsedTime; 
+    vel_angular_derivative = kd_angular*(error_angular - previous_error_angular)/elapsedTime; 
 
     % ------- proporcional 
     vel_linear_proporcional = error_linear*kp_linear;
@@ -72,11 +84,9 @@ while(abs(error_angular) > pi/10 && abs(error_linear) > 1)
     % ------ calculo PID 
     vel_linear = vel_linear_proporcional + vel_linear_integral + vel_linear_derivative; 
     vel_angular = vel_angular_proporcional + vel_angular_integral + vel_angular_derivative; 
-    
-    disp(error_linear);
 
     msg_twist.Linear.X = vel_linear;
-    msg_twist.Angular.Z = 0;
+    msg_twist.Angular.Z = vel_angular;
 
     send(pub_twist,msg_twist);
     
@@ -84,10 +94,30 @@ while(abs(error_angular) > pi/10 && abs(error_linear) > 1)
 
     previous_error_angular = error_angular; 
     previous_error_linear = error_linear;
+    
+    fprintf("-----------------------------------------"); 
+    fprintf("\n");
+    
+    
+    fprintf("velocidade linear: %f", vel_linear); 
+    fprintf("\n");
+    
+    fprintf("velocidade angular: %f", vel_angular); 
+    fprintf("\n");
+    
+    fprintf("erro linear: %f", error_linear); 
+    fprintf("\n");
+    
+    fprintf("erro angular: %f", error_angular); 
+    fprintf("\n");
+    
+    fprintf("-----------------------------------------"); 
+    fprintf("\n");
+    
 
 end
 
-msg_twist.Linear.X =0;
+msg_twist.Linear.X = 0;
 msg_twist.Angular.Z = 0;
     
 send(pub_twist,msg_twist);
